fb8b9d0cd787c95cb2f0cbae0bd2b0614054abf1
Eric Huang <erichuan@andrew.cmu.edu>
2019-04-18 16:38:33 -0400

    updates

--- othello.py
+++ othello.py
@@ -9,6 +9,17 @@ def init(data):
     data.radius = data.cellSize * 0.4
     data.board[3][4] = data.board[4][3] = "black"
     data.board[3][3] = data.board[4][4] = "white"
+    data.numBlack = 0
+    data.numWhite = 0
+    NORTH = (0, -1)
+    SOUTH = (0, +1)
+    EAST = (+1, 0)
+    WEST = (-1, 0)
+    NORTHEAST = (+1, -1)
+    SOUTHEAST = (+1, +1)
+    SOUTHWEST = (-1, +1)
+    NORTHWEST = (-1, -1)
+    data.directions = [NORTH, SOUTH, EAST, WEST, NORTHEAST, SOUTHEAST, SOUTHWEST, NORTHWEST]
     
 def drawBoard(canvas, data):
     for row in range(data.rows):
@@ -22,15 +33,12 @@ def drawCell(canvas, data, row, col):
     y1 = y0 + data.cellSize
     canvas.create_rectangle(x0, y0, x1, y1)
     
-def drawChip(canvas, data):
-    for row in range(data.rows):
-        for col in range(data.cols):
-            if data.board[row][col] == None:
-                cx = data.margin + data.cellSize * (col + 0.5)
-                cy = data.margin + data.cellSize * (row + 0.5)
-                canvas.create_oval(cx-data.radius, cy-data.radius, 
-                                   cx+data.radius, cy+data.radius,
-                                   fill=data.board[row][col]) 
+def drawChip(canvas, data, row, col):
+    cx = data.margin + data.cellSize * (col + 0.5)
+    cy = data.margin + data.cellSize * (row + 0.5)
+    canvas.create_oval(cx-data.radius, cy-data.radius, 
+                        cx+data.radius, cy+data.radius,
+                        fill=data.board[row][col]) 
 
 def drawStartingChips(canvas, data):
     for row in range(3,5):
@@ -41,29 +49,71 @@ def drawStartingChips(canvas, data):
                                cx+data.radius, cy+data.radius,
                                fill=data.board[row][col])
 
+def countBlackChips(data):
+    data.numBlack = 0
+    for row in range(data.rows):
+        for col in range(data.cols):
+            if data.board[row][col] == "black":
+                data.numBlack += 1
+    return data.numBlack
+
+def countWhiteChips(data):
+    data.numWhite = 0
+    for row in range(data.rows):
+        for col in range(data.cols):
+            if data.board[row][col] == "white":
+                data.numWhite += 1
+    return data.numWhite
+
 def drawScore(canvas, data):
     canvas.create_oval(data.width*0.75, 
                        data.margin+2*data.cellSize,
                        data.width*0.75+2*data.radius,
                        data.margin+2*data.cellSize+2*data.radius,
                        fill="black")
+    countBlackChips(data)
     canvas.create_text(data.width*0.75+2*data.radius+20,
                        data.margin+2*data.cellSize+data.radius,
-                       text="x " + str(data.board.count("black")))
+                       text="x " + str(data.numBlack))
     canvas.create_oval(data.width*0.75, 
                        data.margin+4*data.cellSize,
                        data.width*0.75+2*data.radius,
                        data.margin+4*data.cellSize+2*data.radius,
                        fill="white")
+    countWhiteChips(data)
     canvas.create_text(data.width*0.75+2*data.radius+20,
                        data.margin+4*data.cellSize+data.radius,
-                       text="x " + str(data.board.count("white")))
+                       text="x " + str(data.numWhite))
+
+def isLegalMove(data, row, col):
+    legalMoves = []
+    for direction in data.directions:
+        # check if it's out of bounds
+        if ((0 <= row + direction[0] < data.rows) or (0 <= col + direction[1] < data.cols)):
+            return False
+        # check if it's empty space
+        elif data.board[row+direction[0]][col+direction[1]] == None:
+            return False 
+        # check if it's same color piece
+        elif data.board[row][col] == data.board[row+direction[0]][col+direction[1]]:
+            return False
+        # check if it's different color piece and if so, proceed to check if legal move
+        else:
+            if data.board[row+direction[0]*2][col+direction[1]*2] == None:
+                legalMoves.append((row+direction[0]*2, col+direction[1]*2))
+                return True
+            else:
+                return False
+            
 
 def mousePressed(event, data):
     pass
 
 def keyPressed(event, data):
-    pass
+    currRow = (event.x-data.margin)//data.cols
+    currCol = (event.y-data.margin)//data.rows
+    if isLegalMove(data, currRow, currCol):
+        drawChip()
 
 def timerFired(data):
     pass
